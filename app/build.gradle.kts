8ac844d8fdce1225fdae7fc0a306a8d74d33f289
pynicolas <6371790+pynicolas@users.noreply.github.com>
2025-12-05 14:45:10 +0100

    Upgrade AboutLibraries to 13.1.0 (#75)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.aboutLibraries)
+    alias(libs.plugins.aboutLibrariesAndroid)
     alias(libs.plugins.protobuf)
     alias(libs.plugins.kotlin.serialization)
 }
