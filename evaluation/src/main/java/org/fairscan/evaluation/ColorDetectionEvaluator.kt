f06e96d7863a90fb2870e2138cf199ee571953db
Pierre-Yves Nicolas <6371790+pynicolas@users.noreply.github.com>
2026-01-23 18:19:11 +0100

    Update license header

--- evaluation/src/main/java/org/fairscan/evaluation/ColorDetectionEvaluator.kt
+++ evaluation/src/main/java/org/fairscan/evaluation/ColorDetectionEvaluator.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Pierre-Yves Nicolas
+ * Copyright 2025-2026 Pierre-Yves Nicolas
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
