8fae0badb9aec9984c4e826eba1b5cd21c684644
Pierre-Yves Nicolas <6371790+pynicolas@users.noreply.github.com>
2026-04-11 11:34:28 +0200

    Prepare release: v1.19.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // https://ai.google.dev/edge/litert/android/index
         minSdk = 26
         targetSdk = 36
-        versionCode = 66 // increment by 3 because of ABI-specific APKs
-        versionName = "1.18.0"
+        versionCode = 69 // increment by 3 because of ABI-specific APKs
+        versionName = "1.19.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
