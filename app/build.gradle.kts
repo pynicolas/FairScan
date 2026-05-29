d3a28d0eafbfb3963507cafcb661b0faabb77dbe
Pierre-Yves Nicolas <6371790+pynicolas@users.noreply.github.com>
2026-05-29 12:33:05 +0200

    Prepare release: v1.22.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         // https://ai.google.dev/edge/litert/android/index
         minSdk = 26
         targetSdk = 36
-        versionCode = 78 // increment by 3 because of ABI-specific APKs
-        versionName = "1.22.0"
+        versionCode = 81 // increment by 3 because of ABI-specific APKs
+        versionName = "1.22.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
